ab42c2afdad0f0c962dabf773c15a698c547eae6
Chaila1 <115548186+Chaila1@users.noreply.github.com>
2024-03-14 10:18:20 +0000

    commit 2

---
+++ PlaylistApp/src/playlistapp/Song.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package playlistapp;
+
+/**
+ *
+ * @author Matty School
+ */
+public class Song {
+    private String songName, songGenre;
+
+    public Song() {
+        songName = new String();
+        songGenre =  new String();
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public void setSongName(String songName) {
+        this.songName = songName;
+    }
+
+    public String getSongGenre() {
+        return songGenre;
+    }
+
+    public void setSongGenre(String songGenre) {
+        this.songGenre = songGenre;
+    }
+    
+    
+    
+}
